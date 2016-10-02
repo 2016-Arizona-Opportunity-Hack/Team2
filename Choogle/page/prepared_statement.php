46aec7e3030675d9dea2e3bed015e2bba2e8f211
Tyman Sin <sin.tyman@yahoo.com>
2016-10-02 05:08:41 -0700

    Documentation
    Added comments

--- Choogle/page/prepared_statement.php
+++ Choogle/page/prepared_statement.php
@@ -1,4 +1,5 @@
 <?php
+	//pretty much useless at this point
 	$prepstat = $conn->prepare(SELECT /*result*/ FROM /*link_table*/ WHERE /*condition*/();
 	$prepstat = $bindParam($keywords);
 
--- Choogle/page/search_results.php
+++ Choogle/page/search_results.php
@@ -17,6 +17,7 @@
 	</head>
 	<body>
 		<?php
+			//use of prepared statement and htmlentities to protect agains XSS and SQL Injection
 			$mysqli = mysqli_connect("localhost", "root", "", "site_list") or die('Error loading database');
 			$query = "SELECT * FROM site WHERE Name = '$[keywords]'";
 			if($stmt = $mysqli->prepare($query)){
@@ -30,21 +31,13 @@
 			}
 			$mysqli->close();
 			
-			/* $mysqli = mysqli_connect("localhost", "root", "", "site_list") or die('Error loading database');
-			$query = "SELECT Name FROM site";
-			if($stmt = $mysqli->prepare($query)){
-				$stmt-> execute();
-				$stmt->bind_result($name);
-				while($stmt->fetch()){
-					echo "<h1>$name</h1>";
-				}
-				$mysqli->close();
-			} */
+			//Calling reusable code
 			require_once("menu.php");
 			require_once("search_bar.php");	
 			require_once("nav.php");
 		?>
 		
+		<!--iframe to embed other websites-->
 		<div id="frame">
 			<?php
 				$input = $_GET['search_in'];
