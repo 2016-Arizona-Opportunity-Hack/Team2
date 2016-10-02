60af04b8b110efddbfd51f4d36b358b3d9c1bc7b
Maurice Ajluni <ajluni39@gmail.com>
2016-10-02 01:05:30 -0700

    Add thank you message after feedback, and format the text

---
+++ Choogle/page/feedback_submit.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Connect to Chandler - Forum</title>
+		<meta charset="ISO-8859-1">
+		<link rel="stylesheet" href="../style/style.css">
+		<script src="../script/dropdown.js"></script>
+	</head>
+	<body>
+		<?php
+			require_once("menu.php");
+			require_once("nav.php");
+		?>
+		
+		<h2>Thank you for your submition, it has been recorded.</h2>
+		<br>
+		<a href = "index.php" name = "home">Return to home page</a> 
+	</body>
+</html>
